63040ede1ee510e81fa991b7539e993ed32cd3f4
EdgarCordex <89655037+EdgarCordex@users.noreply.github.com>
2021-08-31 12:26:16 -0500

    Update and rename assignments/21LevaduraParaPizza/src/exercise.py to asignaciones/21LevaduraParaPizza/src/exercise.py

---
+++ asignaciones/21LevaduraParaPizza/src/exercise.py
@@ -0,0 +1,13 @@
+def main():
+    # escribe tu código abajo de esta línea
+    pass
+harina= float(input("Harina en gramos:"))
+
+# Operaciones
+levadura= (harina*50)/1000
+
+# Resultado
+print("gramos de levadura necesarios:")
+
+if __name__ == '__main__':
+    main()
