d5cda928909af94d7474cd08492a2cbb23f3669a
EdgarCordex <89655037+EdgarCordex@users.noreply.github.com>
2021-08-31 12:21:31 -0500

    Update and rename assignments/09Telefono/src/exercise.py to asignaciones/09Telefono/src/exercise.py

---
+++ asignaciones/09Telefono/src/exercise.py
@@ -0,0 +1,15 @@
+def main():
+    # escribe tu código abajo de esta línea
+    pass
+texts= int(input("Numero de mensajes:"))
+megas= float(input("Numero de megas:"))
+minutes= int(input("Numero de minutos:"))
+
+# Formula del costo
+costo= float(mensaje+megas+minutos)*0.80
+
+# Muestra del resultado final al usuario 
+print("El costo por mes sera:", costo)
+
+if __name__ == '__main__':
+    main()
