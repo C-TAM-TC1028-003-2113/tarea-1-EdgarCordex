ad175b56b512a580f7c6e057fce89327ee0f0338
EdgarCordex <89655037+EdgarCordex@users.noreply.github.com>
2021-08-31 12:22:42 -0500

    Update and rename assignments/10Videojuego/src/exercise.py to asignaciones/10 Videojuego/src/exercise.py

---
+++ asignaciones/10 Videojuego/src/exercise.py
@@ -0,0 +1,14 @@
+def main():
+    # escribe tu código abajo de esta línea
+    pass
+new= int(input("Cantidad de juegos nuevos:"))
+used= int(input("Cantidad de juegos usados:"))
+
+#Costo total por los juegos
+total= (nuevo*1000)+(usado*350)
+
+#Resultado final
+print("Total de la compra:", total)
+
+if __name__ == '__main__':
+    main()
